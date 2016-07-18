053003c459189c99dd20dc3caa410ce9d4b741d0
Natsuki Tanaka <natsukia3@gmail.com>
2016-07-18 17:10:30 +0900

    Add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true, length: { maximum: 20}
+  validates :username, presence: true, length: { maximum: 15}, uniqueness: { case_sensitive: false }
+  validates :description, length: { maximum: 160 }
 end
