159ff7195cbcc382bb89bf67adc5bcb62d7a12d5
Natsuki Tanaka <natsukia3@gmail.com>
2016-07-19 23:01:09 +0900

    Sign in with email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
+
   validates :name, presence: true, length: { maximum: 20}
   validates :username, presence: true, length: { maximum: 15}, uniqueness: { case_sensitive: false }
   validates :description, length: { maximum: 160 }
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
