18776359e322b61156429312831f9196886e030f
Natsuki Tanaka <natsukia3@gmail.com>
2016-07-24 23:45:43 +0900

    User profile update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,22 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update_attributes(user_params)
+      flash[:notice] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :username, :description,
+                                 :avatar, :location, :website_url)
   end
 end
