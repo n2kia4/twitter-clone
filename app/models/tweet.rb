3e5cc1c8af63f5cc10a63bd0d12fb2a3c99523c7
Natsuki Tanaka <natsukia3@gmail.com>
2016-08-01 00:36:34 +0900

    Change order of tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,6 @@ class Tweet < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 }
 
   mount_uploader :photo, PhotoUploader
+
+  default_scope -> { order(created_at: :desc) }
 end
